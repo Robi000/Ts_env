75cd74b762972f8e3b5b4734552a14178fa0b9d9
Robi000 <88291984+Robi000@users.noreply.github.com>
2023-11-15 00:57:26 +0300

    the complition part is done

--- CERT/urls.py
+++ CERT/urls.py
@@ -1,4 +1,9 @@
 from django.urls import path
-from .views import Payment, payment_handler
+from .views import Payment, payment_handler, complition, complition_handler
 
-urlpatterns = [path("", Payment), path("payment/", payment_handler)]
+urlpatterns = [
+    path("", Payment),
+    path("payment/", payment_handler),
+    path("complition/", complition),
+    path("complition/complitionhandler/", complition_handler),
+]
--- CERT/views.py
+++ CERT/views.py
@@ -21,19 +21,60 @@ def Payment(request):
 
 
 def payment_handler(request):
-    print("i was here")
-    tuple_list = list(request.POST.items())
     context = {}
-    print(tuple_list)
     Students = Student.objects.filter(Batch=int(request.POST["Batch"]))
+    count = 0
     for stu in Students:
         print(stu.id)
         if stu.payment_status != request.POST.get(str(stu.id), stu.payment_status):
             stu.payment_status = request.POST.get(str(stu.id), stu.payment_status)
             print("changed")
             stu.save()
+            count += 1
     context["stus"] = Students
     context["Batch"] = int(request.POST["Batch"])
-    context["message"] = "scussessfully changed"
+    if count > 0:
+        context["message"] = "scussessfully changed"
     context["id"] = True
     return render(request, "p/payment.html", context)
+
+
+def complition(request):
+    max_value = Student.objects.aggregate(Max("Batch"))["Batch__max"]
+    lis = list(range(max_value + 1))
+    context = {"Batch": lis[1:]}
+
+    if request.htmx:
+        print(request.POST)
+        Students = Student.objects.filter(Batch=int(request.POST["Batch"]))
+        context["stus"] = Students
+        context["Batch"] = int(request.POST["Batch"])
+        return render(request, "p/complition.html", context)
+    Students = Student.objects.filter(Batch=1)
+    context["stus"] = Students
+    return render(request, "complition.html", context)
+
+
+def complition_handler(request):
+    dic = {"on": True, "off": False}
+    context = {}
+    Students = Student.objects.filter(Batch=int(request.POST["Batch"]))
+    count = 0
+    for stu in Students:
+        if request.POST.get(str(stu.id), False):
+            if not stu.finished:
+                stu.finished = True
+                stu.save()
+                count += 1
+        else:
+            if stu.finished:
+                stu.finished = False
+                stu.save()
+                count += 1
+
+    context["stus"] = Students
+    context["Batch"] = int(request.POST["Batch"])
+    if count > 0:
+        context["message"] = "scussessfully changed"
+    context["id"] = True
+    return render(request, "p/complition.html", context)
